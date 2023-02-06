23bc1e1554c9c0bef1add24c532920154da6accf
EmmaKuo <35401897+EmmaKuo@users.noreply.github.com>
2023-02-07 02:40:47 +0800

    add code

---
+++ sketch.js
@@ -0,0 +1,63 @@
+/*
+  Forest
+  Based on work by Lior Ben-Gai
+*/
+
+function setup(){
+  createCanvas(1280,320);
+  background(320);
+  stroke(100);
+  fill(200);
+}
+
+function draw(){
+  drawTree();
+
+  if(frameCount % 7 == 0)drawFog(); 
+}
+
+
+function drawFog(){
+  push();
+  fill(32, 16);
+  noStroke();
+  rect(0,0,width,height);
+
+  pop();
+}
+
+
+function drawTree(){
+    let bLen = random(300,10);
+    let bAng = PI*0.1;
+
+    push();
+    translate(random(width), height);
+    branch(bLen, -bAng); 
+    pop();
+}
+
+
+function branch(len, theta){
+  push();
+  rotate(theta);
+  strokeWeight(sqrt(len)*0.11);
+  stroke(255,255,0);
+  line(0,0, len, 0);
+  stroke(100);
+  ellipse(0,0,len*0.1,len);
+  noStroke();
+  fill(204,153,255);
+  rect(0, 20, len*0.05, len*0.05);
+  translate(len,0); 
+
+  if(len > 4.0){ 
+    let newAng = random(PI*0.25); 
+    let newLen = len * random(0.4,0.8); 
+    branch(len * random(0.4,0.8), - newAng);
+    branch(len * random(0.4,0.8), newAng); 
+  }else{
+    rect(0, 20, len*0.01, len*0.01); 
+  }
+  pop();
+}
